8cb16c099122ba8e4b697e3856a5b56531f1ea7b
venucia <xiebao0301@163.com>
2019-05-21 17:46:07 +0800

    增加登陆功能和登陆背景图

---
+++ src/view/login/LoginForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Form, Icon, Input, Button } from 'antd'
+
+
+export default class LoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.onLoginOk()
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: '请输入账号!' }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="账号"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: '请输入密码!' }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="密码"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button
+            style={{ width: '100%' }}
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    )
+  }
+}
--- src/view/login/LoginView.jsx
+++ src/view/login/LoginView.jsx
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
-import {Button} from 'antd'
+import React from 'react'
+import { Form, Card, message } from 'antd'
+import LoginFromClass from './LoginForm'
+import HttpApi from '../util/HttpApi'
 
-class LoginView extends Component {
-    render() {
-        return (
-            <div>
-                登录界面
-                <Button onClick={this.loginHandler} type='primary'>登录测试</Button>
-            </div>
-        );
-    }
+const LoginFrom = Form.create({ name: 'normal_login' })(LoginFromClass)
 
-    loginHandler=()=>{
-        this.props.history.push('/mainView')
-    }
+export default class LoginView extends React.Component {
+  onLoginOk = e => {
+    this.refs.form.validateFields((error, values) => {
+      if (!error) {
+        HttpApi.getUserInfo(values, doc => {
+          if (doc.data.code === 0 && doc.data.data.length > 0) {
+            // 登录成功
+            this.props.history.push('/mainView')
+          } else {
+
+            message.error("用户名/密码错误")
+          }
+        })
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Card title="登录" style={{ width: 400, height: 310 }}>
+          <LoginFrom ref="form" onLoginOk={this.onLoginOk} />
+        </Card>
+      </div>
+    )
+  }
 }
-
-export default LoginView;
