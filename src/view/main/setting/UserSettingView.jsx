8c0821a225c37393db27192deb7a8d89c90f214b
fc <1018555611@qq.com>
2019-07-12 21:28:07 +0800

    版本号 改为 v 1.0.1

--- src/view/main/setting/UserSettingView.jsx
+++ src/view/main/setting/UserSettingView.jsx
@@ -10,7 +10,7 @@ export default function UserSettingView(props) {
             { title: '姓名', description: userinfo.name },
             { title: '职位', description: userinfo.remark },
             { title: '手机号', description: userinfo.phonenumber },
-            { title: '版本号', description: 'V 1.0.0' }
+            { title: '版本号', description: 'V 1.0.1' }
         ]
         setData(listData)
     }, [])
