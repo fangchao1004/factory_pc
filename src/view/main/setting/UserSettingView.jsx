522f6063cd174c787bf901148629da77eadbc8ae
fc <1018555611@qq.com>
2019-09-24 14:43:46 +0800

    补充提交

--- src/view/main/setting/UserSettingView.jsx
+++ src/view/main/setting/UserSettingView.jsx
@@ -10,11 +10,12 @@ export default function UserSettingView(props) {
             { title: '姓名', description: userinfo.name },
             { title: '职位', description: userinfo.remark },
             { title: '手机号', description: userinfo.phonenumber },
-            { title: '版本号', description: 'V 1.1.0' },
+            { title: '版本号', description: 'V 1.1.1' },
             // { title: '更新', description: '1.0.7支持缺陷数据导出为Excel' }
             // { title: '更新', description: '1.0.8 添加考勤信息-测试版本' }
             // { title: '更新', description: '1.0.9 添加设备信息修改功能' }
-            // { title: '更新', description: '消费审批' }
+            // { title: '更新', description: '1.1.0 消费审批' }
+            // { title: '更新', description: '1.1.1 任务进程添加' }
         ]
         setData(listData)
     }, [])
