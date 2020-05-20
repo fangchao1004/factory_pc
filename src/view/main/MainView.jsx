c9e0b6eb94efc1b2311b4f4e17d01af4acad404c
fc <1018555611@qq.com>
2020-05-20 17:15:50 +0800

    bug修改

--- src/view/main/MainView.jsx
+++ src/view/main/MainView.jsx
@@ -223,7 +223,7 @@ export default class MainView extends Component {
                             <span>
                                 <Icon type="reconciliation" />
                                 <span>缺陷</span>
-                                <Badge dot={this.state.aboutMeBugNum + this.state.runBugNum > 0} style={{ marginLeft: 30 }} />
+                                <Badge dot={JSON.parse(localUserInfo).permission && JSON.parse(localUserInfo).permission.split(',').indexOf('1') !== -1 ? (this.state.aboutMeBugNum + this.state.runBugNum) > 0 : this.state.aboutMeBugNum > 0} style={{ marginLeft: 30 }} />
                             </span>
                         }>
                             <Menu.Item key="/mainView/bugAboutMe">
