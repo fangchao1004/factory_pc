669e8bb606835afe26bc9799362f1e81687f85e3
fc <1018555611@qq.com>
2020-12-17 16:20:44 +0800

    添加登录日志的条件查询功能

--- src/view/main/userLoginLogMode/UserLoginLogsView.jsx
+++ src/view/main/userLoginLogMode/UserLoginLogsView.jsx
@@ -1,13 +1,14 @@
-import { Badge, Table } from 'antd'
+import { Badge, Button, Col, Form, Row, Table, Select } from 'antd'
 import React, { useCallback, useEffect, useState } from 'react'
 import HttpApi from '../../util/HttpApi'
 import moment from 'moment'
+const { Option } = Select;
 
 const totalCount = 0
-const condition = {
+var condition = {
     page: 1
 }
-
+var searchCondition = {}
 export default () => {
     const [dataSource, setDataSource] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
@@ -42,6 +43,13 @@ export default () => {
 
     return (
         <div style={styles.root}>
+            <div style={styles.header}>
+                <Searchfrom startSearch={async (conditionValues) => {
+                    searchCondition = conditionValues;
+                    let tempC = { ...conditionValues, ...condition }
+                    loadData(tempC)
+                }} />
+            </div>
             <div style={styles.body}>
                 <Table
                     size="small"
@@ -78,36 +86,81 @@ export default () => {
                             width: 100,
                             align: 'center',
                             render: (val, record) => {
-                                let is_warning = false;
-                                if (record.remark && record.remark.indexOf('注意') !== -1) {
-                                    is_warning = true;
-                                }
-                                return <Badge status={val === 0 ? is_warning ? 'warning' : 'success' : 'error'} />
+                                return <Badge status={val === 0 ? 'success' : 'error'} />
                             }
                         },
                         {
                             title: '登录备注',
                             dataIndex: 'remark',
-                            key: 'remark'
+                            key: 'remark',
+                            render: (val, record) => {
+                                if (record.uuid) { val = val + '【手机端】' }
+                                return val
+                            }
                         }
                     ]}
                     dataSource={dataSource}
                     pagination={pagination}
                     onChange={p => {
                         condition.page = p.current
-                        loadData(condition)
+                        loadData({ ...condition, ...searchCondition })
                     }}
                 />
             </div>
         </div>
     )
 }
+const Searchfrom = Form.create({ name: 'form' })(props => {
+    const itemProps = { labelCol: { span: 6 }, wrapperCol: { span: 18 } }
+    return <Form onSubmit={(e) => {
+        e.preventDefault();
+        props.form.validateFields((err, values) => {
+            ///values搜寻条件数据过滤
+            let newObj = {};
+            for (const key in values) {
+                if (values.hasOwnProperty(key)) {
+                    const element = values[key];
+                    if (element !== undefined && element !== null) {
+                        newObj[key] = values[key]
+                    }
+                }
+            }
+            props.startSearch(newObj);
+        });
+    }}>
+        <Row>
+            <Col span={6}>
+                <Form.Item label='登录状态'  {...itemProps}>
+                    {props.form.getFieldDecorator('status', {
+                        rules: [{ required: false }]
+                    })(
+                        <Select style={{ width: '100%' }} allowClear>
+                            <Option value={0}><Badge status={'success'} />正常</Option>
+                            <Option value={1}><Badge status={'error'} />拒绝</Option>
+                        </Select>
+                    )}
+                </Form.Item>
+            </Col>
+            <Col span={18}>
+                <div style={{ textAlign: 'right', paddingTop: 3 }}>
+                    <Button icon='search' type="primary" htmlType="submit">查询</Button>
+                    <Button icon='delete' style={{ marginLeft: 8 }} onClick={() => { props.form.resetFields() }}>清除</Button>
+                </div>
+            </Col>
+        </Row>
+    </Form>
+})
 const styles = {
     root: {
         backgroundColor: '#F1F2F5',
         width: '100%',
         height: '100vh',
         padding: 10,
+    },
+    header: {
+        backgroundColor: '#FFFFFF',
+        padding: '16px 16px 0 16px',
+        marginBottom: 10,
     },
     body: {
         backgroundColor: '#FFFFFF',
