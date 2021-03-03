92efc321b2426fc02dd9edc915e00d7058ec2bd7
August <1018555611@qq.comm>
2021-03-04 00:02:55 +0800

    工作票处理流程，设定默认选择项；bug修改

--- src/view/main/jobTicketMode/JobTicketDrawer.jsx
+++ src/view/main/jobTicketMode/JobTicketDrawer.jsx
@@ -13,11 +13,12 @@ var step_des = '';
 var ticketNextUserNameList = [];
 export default function JobTicketDrawer({ isAgent, visible, onClose, record, resetData }) {
     // console.log('record:', record);
+
     const [stepLogVisible, setStepLogVisible] = useState(false);///展示步骤界面
     const [currentJobTicketValue, setCurrentJobTicketValue] = useState({})///填写改动后的数值- 提交时使用
     const [currentUser] = useState(JSON.parse(storage.getItem('userinfo')))
     const [userList, setUserList] = useState([])
-    const [selectValue, setSelectValue] = useState(null)
+    const [selectValue, setSelectValue] = useState('1')
 
     const [actionSelectAble, setActionSelectAble] = useState(true)///处理选项是否可以操作
     const [userSelectAble, setUserSelectAble] = useState(true)///人员项是否可以操作
@@ -65,6 +66,7 @@ export default function JobTicketDrawer({ isAgent, visible, onClose, record, res
     const init = useCallback(async () => {
         // console.log('init');
         if (record && record.job_t_r_id) {
+            setSelectValue('1')
             let res = await HttpApi.getJTRecords({ id: record.job_t_r_id })
             if (res.data.code === 0) {
                 let tempObj = JSON.parse(JSON.stringify(res.data.data[0]))
@@ -104,7 +106,7 @@ export default function JobTicketDrawer({ isAgent, visible, onClose, record, res
                     setShowBackOption(true)///可以打回
                     setShowStopBtn(true)///可以作废
                     runUserlist(user_list, isAgent ? 10 : 11)///运行值长名单[针对下一步5-待完结]
-                } else if (record.status === 5) {///当前待完结和完结时
+                } else if (record.status === 5) {///当前待完结
                     setShowStopBtn(true)///可以作废
                     setShowPrintBtn(true)///可以打印
                     if (!isAgent) setUserSelectAble(false)///处理情况下不可选择人员
@@ -147,6 +149,9 @@ export default function JobTicketDrawer({ isAgent, visible, onClose, record, res
     const init2 = useCallback(() => {
         if (!isAgent && selectValue === '1' && record && record.is_sub === 0 && record.status === 2) setTakeTicketAndPrint(true)///处理 下一步时 可以【提交打印】
         else { setTakeTicketAndPrint(false) }
+        if (selectValue === '1' && record && record.status >= 1) {
+            step_des = changeShowLabByStauts(record.status, record.is_sub)
+        } else if (selectValue === '-1') { step_des = '打回' }
     }, [isAgent, selectValue, record])
 
     const resetHandler = useCallback(() => {
@@ -428,10 +433,10 @@ export default function JobTicketDrawer({ isAgent, visible, onClose, record, res
                                                     current_step_user_id_list_temp = ',' + record.per_step_user_id + ','
                                                 }
                                                 let is_read = 0;
-                                                if (record.is_sub === 1) {
-                                                    is_read = record.status === 5 ? 1 : 0
+                                                if (record.is_sub !== 0) {
+                                                    is_read = record.status === 5 ? 1 : 0 ///措施票状态5时，一下步就是6完结了 不需要在read重置
                                                 } else {
-                                                    is_read = record.status === 3 ? 1 : 0
+                                                    is_read = record.status === 3 ? 1 : 0 ///主票状态3时，一下步就是4完结了 不需要在read重置
                                                 }
                                                 ///每次处理 未读重置
                                                 let newJTAR_data = {
@@ -464,12 +469,14 @@ export default function JobTicketDrawer({ isAgent, visible, onClose, record, res
                                                         }
                                                     } else {///主票
                                                         if (selectValue === '1') {///下一步
+                                                            console.log('aaaapdasdasd step_desstep_desstep_des', step_des);
                                                             if (record.status === 3) {///当前是3 下一步就是4
                                                                 obj['step_des'] = step_des
                                                             } else {
                                                                 obj['step_des'] = step_des + '至 ' + ticketNextUserNameList.join(',')
                                                             }
                                                         } else {///打回
+                                                            console.log('asdasdaaaaqqq123123123s', step_des);
                                                             obj['step_des'] = step_des + '至 ' + record.per_step_user_name
                                                         }
                                                     }
--- src/view/main/jobTicketMode/JobTicketOfMy.jsx
+++ src/view/main/jobTicketMode/JobTicketOfMy.jsx
@@ -179,7 +179,7 @@ export default function JobTicketOfMy() {
             title: '操作', dataIndex: 'action', key: 'action', align: 'center', width: 100, render: (_, record) => {
                 let is_over = false
                 if (record.is_sub !== 1 && record.status === 4) { is_over = true }
-                else if (record.is_sub === 1 && record.status === 5) { is_over = true }///是否完结
+                else if (record.is_sub === 1 && record.status === 6) { is_over = true }///是否完结
                 let inCurrentUserList = false
                 inCurrentUserList = record.current_step_user_id_list.indexOf(`,${currentUser.id},`) !== -1;///是否为当前处理人
                 return <div>
@@ -225,7 +225,7 @@ export default function JobTicketOfMy() {
                         // console.log('props.record:',props.record);
                         let is_over = false
                         if (props.record.is_sub !== 1 && props.record.status === 4) { is_over = true }
-                        else if (props.record.is_sub === 1 && props.record.status === 5) { is_over = true }///是否完结
+                        else if (props.record.is_sub === 1 && props.record.status === 6) { is_over = true }///是否完结
                         let topAdd = props.record.is_sub === 0 && !is_over && (hasFixPer || hasManagerPer) ? ///主票未完结时且有维修或专工权限
                             <div key={'x'}>
                                 <Tooltip title={'新增措施票'} placement="left">
